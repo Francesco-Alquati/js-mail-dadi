c25d2611354d9998badb3ecfc4568b4f4ee108f3
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-06-28 18:28:52 +0200

    [UPD] Dice excercise Done

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,5 +1,18 @@
 // CREO LA VARIABILE PER IL LANCIO DEL DADO DEL GIOCATORE
 let player_dice = Math.floor((Math.random() * 6 + 1 ));
+console.log('player: ' + player_dice);
 
 // CREO LA VARIABILE PER IL LANCIO DEL DADO DEL COMPUTER
 let pc_dice = Math.floor((Math.random() * 6 + 1 ));
+console.log('pc: ' + pc_dice);
+
+// CREO UN IF PER CONFRONTARE LE DUE VARIABILI 
+if(player_dice > pc_dice) {
+    console.log('Hai vinto');
+}
+else if(player_dice < pc_dice) {
+    console.log('Hai perso');
+}
+else {
+    console.log('Pareggio');
+}
