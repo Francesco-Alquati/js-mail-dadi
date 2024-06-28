7c04d552db1e634b9f8fb100348cbe541fad28be
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-06-28 18:21:22 +0200

    [UPD] Created varable for pc throwing dice

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,2 +1,5 @@
 // CREO LA VARIABILE PER IL LANCIO DEL DADO DEL GIOCATORE
 let player_dice = Math.floor((Math.random() * 6 + 1 ));
+
+// CREO LA VARIABILE PER IL LANCIO DEL DADO DEL COMPUTER
+let pc_dice = Math.floor((Math.random() * 6 + 1 ));
