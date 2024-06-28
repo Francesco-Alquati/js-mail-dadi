7967fdfb7d429d8ae352de08cb2a9101def926d7
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-06-28 17:35:02 +0200

    [UPD] get button from DOM

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,3 +1,10 @@
 // CREO IL MIO ARRAY DI EMAIL
-
 const saved_email = ["francesco.alquati@tiscali.it", "alquati.francesco@tiscali.it", "francesco.alquati@gmail.com", "alquati.francesco@gmail.com", "pippo@tiscali.it", "pluto@tiscali.it", "pape.rino@tiscali.it", "mario@gmail.com", "maria@gmail.com", "luca@gmail.com",];
+
+// RECUPERO IL PULSANTE DAL DOM
+const button = document.getElementById('search');
+
+button.addEventListener('click', function (){
+
+
+})
