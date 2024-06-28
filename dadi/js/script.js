cb6415c0f0ce88b0048d6fec302b70ed77cba859
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-06-28 18:56:16 +0200

    [UPD] Added some style to Dice Game exercise

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,3 +1,7 @@
+const button = document.getElementById('play-game');
+
+button.addEventListener('click', function (){
+
 // CREO LA VARIABILE PER IL LANCIO DEL DADO DEL GIOCATORE
 let player_dice = Math.floor((Math.random() * 6 + 1 ));
 console.log('player: ' + player_dice);
@@ -9,10 +13,18 @@ console.log('pc: ' + pc_dice);
 // CREO UN IF PER CONFRONTARE LE DUE VARIABILI 
 if(player_dice > pc_dice) {
     console.log('Hai vinto');
+    document.getElementById('result').innerHTML = `Hai vinto`;
 }
 else if(player_dice < pc_dice) {
     console.log('Hai perso');
+    document.getElementById('result').innerHTML = `Hai perso`;
 }
 else {
     console.log('Pareggio');
+    document.getElementById('result').innerHTML = `Pareggio`;
 }
+
+document.getElementById('player').innerHTML = `${player_dice}`;
+document.getElementById('pc').innerHTML = `${pc_dice}`;
+})
+
